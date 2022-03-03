4ddbdd77ec249a6474ba075fd7bf9e136e8c86a9
Kevin Kabore <kevin.s.kabore@gmail.com>
2022-03-02 22:56:57 -0500

    feat(form): fetch collection based on form input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useAsync} from './utils/hooks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'https://api.genie.xyz'
+
+function fetchCollection(address: string) {
+  const options = {
+    filters: {address},
+    limit: 1,
+    // fields: {stats: 1},
+  }
+  return fetch(`${API_URL}/collections`, {
+    method: 'POST',
+    body: JSON.stringify(options),
+  })
 }
 
-export default App;
+function App() {
+  const [address, setAddress] = React.useState('')
+  const {status, data, error, run} = useAsync()
+  //TODO: Actually paginate the results
+  let paginatedData = data.slice(0, 10)
+
+  return (
+    <div className="App">
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          run(fetchCollection(address))
+        }}
+      >
+        <label htmlFor="collection-address-input">Collection Address</label>
+
+        <input
+          id="collection-address-input"
+          name="collectionAddress"
+          placeholder="Collection Address"
+          onChange={e => setAddress(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        Colection Data
+        {status === 'pending' && <p>Loading...</p>}
+        {status === 'resolved' &&
+          paginatedData.map((item: any) => (
+            <div key={item._id}>
+              <img src={item.imageUrl} alt={item.name} />
+            </div>
+          ))}
+        {status === 'rejected' && <p>{error?.message}</p>}
+      </div>
+    </div>
+  )
+}
+
+export default App
